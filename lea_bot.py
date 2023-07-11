4854af216eb0b284e5a3e41d99da8f5f28b1d544
Andrew Ton That <vinh@Euphrates>
2023-07-11 04:04:26 -0700

    rewrote bot to do cogs instead of messy function code

---
+++ lea_bot.py
@@ -0,0 +1,45 @@
+import os, discord
+from dotenv import load_dotenv
+from discord.ext import commands, tasks
+import logging
+
+from cogs.Translate import Translate
+
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+INTENTS = discord.Intents.default()
+INTENTS.members = True
+INTENTS.typing = False
+INTENTS.presences = False
+INTENTS.message_content = True
+
+PREFIX = "l."
+
+bot = commands.Bot(command_prefix = PREFIX, intents = INTENTS)
+
+
+@bot.event
+async def on_ready():
+    print('Adding cogs...')
+    await bot.add_cog(Translate(bot))
+
+    print('Logged in as {0}!'.format(bot.user))
+
+    return
+
+
+
+@bot.event
+async def on_message(message):
+    print(str(message.author) + ": " + message.content)
+
+    await bot.process_commands(message)
+
+    return
+
+    
+
+
+bot.run(TOKEN)
