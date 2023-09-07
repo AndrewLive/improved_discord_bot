42c8a461f5e34f8a17a4fbafd7110560a3260619
Andrew <andrew@puresynergy.com>
2023-09-06 20:25:18 -0700

    attempting real time log reading with Watchdog and multiprocessing. will prob switch to async

--- cogs/Minecraft.py
+++ cogs/Minecraft.py
@@ -1,7 +1,13 @@
 import os, discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from mcstatus import JavaServer
 from dotenv import load_dotenv
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+from multiprocessing import Process
+import re
+import asyncio
 
 class Minecraft(commands.Cog):
     def __init__(self, bot):
@@ -17,6 +23,12 @@ class Minecraft(commands.Cog):
             self.query = self.server.query()
         except:
             print("server cannot be reached at this time")
+
+        # read chat real time
+        self.observerProcess = Process(target=self.observerLoop)
+        self.observerProcess.start()
+
+
 
     @commands.command(aliases=["mc"])
     async def minecraft(self, ctx, *args):
@@ -47,14 +59,14 @@ class Minecraft(commands.Cog):
 
 
     @commands.command(aliases=["rcon"])
-    async def rconsay(self, ctx, *args):
+    async def rconsay(self, ctx, *, args=None):
         try:
             self.server.ping()
         except:
             ctx.send("Unable to reach server")
             return
         
-        msg = ' '.join(args)
+        msg = f'{"".join(args)}'
         command = f'mcrcon -H {self.ip} -P {self.port} -p {self.rconPass} "say <Discord: {ctx.message.author.name}> {msg}"'
         os.system(command)
 
@@ -62,3 +74,45 @@ class Minecraft(commands.Cog):
 
         await ctx.send(f'Sent message to server: <Discord: {ctx.message.author.name}> {msg}')
         return
+
+
+
+    # TO DO: add ability to communicate from minecraft to discord
+    # Read output logs of mc server to do this
+    # Or try PyMChat
+    class MyHandler(FileSystemEventHandler):
+        def on_modified(self, event):
+            print(f'event type: {event.event_type}  path : {event.src_path}')
+            if event.src_path == "/mnt/c/Users/Vinh/DarkRPG/data/logs/latest.log":
+                with open('/mnt/c/Users/Vinh/DarkRPG/data/logs/latest.log', 'r') as f:
+                    line = f.readlines()[-1].strip('\n')
+                    msg = line[33:]
+
+                print(f'Line: {line}')
+                print(f'Msg: {msg}')
+                if re.match(r"<.+> .+", msg):
+                    print('Valid message!')
+                if re.match(r"<.+> l\.chat .+", msg):
+                    print('Valid command!')
+
+
+    def observerLoop(self):
+        path = (r"/mnt/c/Users/Vinh/DarkRPG/data/logs")
+        event_handler = self.MyHandler() 
+        observer = Observer()
+        observer.schedule(event_handler, path, recursive=False)
+
+        observer.start()  #for starting the observer thread
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        return
+            
+    
