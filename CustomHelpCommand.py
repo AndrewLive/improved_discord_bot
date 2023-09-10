4f7845fb05dce847dcf5ff6399b61f7622260e2f
Andrew <andrew@puresynergy.com>
2023-09-10 01:01:57 -0700

    fixed errors and overrided help

--- CustomHelpCommand.py
+++ CustomHelpCommand.py
@@ -9,14 +9,19 @@ class CustomHelpCommand(commands.HelpCommand):
     def __init__(self):
         super().__init__()
 
-    async def send_bot_help(self, mapping: Mapping[Cog | None, List[Command[Any, Callable[..., Any], Any]]]) -> None:
-        return await super().send_bot_help(mapping)
+    async def send_bot_help(self, mapping: Mapping):
+        for cog in mapping:
+            if cog == None:
+                continue
+            await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]}')
+        
+        return
 
     async def send_cog_help(self, cog: Cog) -> None:
-        return await super().send_cog_help(cog)
+        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]}')
     
-    async def send_group_help(self, group: Group[Any, Callable[..., Any], Any]) -> None:
+    async def send_group_help(self, group: Group):
         return await super().send_group_help(group)
     
-    async def send_command_help(self, command: Command[Any, Callable[..., Any], Any]) -> None:
-        return await super().send_command_help(command)
+    async def send_command_help(self, command: Command):
+        await self.get_destination().send(command.name)
