302fa05ab16382ffd10e893e57e3ef5ce6aa10ac
Andrew <andrew@puresynergy.com>
2023-09-10 22:57:01 -0700

    Changed command arguments

--- cogs/Translate.py
+++ cogs/Translate.py
@@ -11,53 +11,47 @@ class Translate(commands.Cog):
         self.cog_translator = Translator()
 
     @commands.command()
-    async def translate(self, ctx, *args):
+    async def translate(self, ctx, srclang=None, destlang='en', *message):
         """
         Translates the message the invoker replied to
-
-        Usage:
-            l.translate <src> <dest>
 
         Note:
             Input languages must be in Google Language Code format (https://developers.google.com/admin-sdk/directory/v1/languages)
         
         Arguments:
-            src: Language of the original message. Auto detects language if none given
-            dest: Intended destination language. Defaults to English
+            srclang: Language of the original message. Auto detects language if none given
+            destlang: Intended destination language. Defaults to English
+            message: The message you want to translate. If none specified, will translate the replied-to message
         """
 
         # print('Translate Command!')
-        if ctx.message.reference == None:
-            await ctx.reply("Error: No message to translate", mention_author=False)
-            return
-        
-        if len(args) > 3:
-            await ctx.reply("Error: Too many arguments specified")
-            return
+        source_message = " ".join(message)
+        if message == ():
+            if ctx.message.reference == None:
+                await ctx.reply("Error: No message to translate", mention_author=False)
+                return
+            source = await ctx.fetch_message(ctx.message.reference.message_id)
+            source_message = source.content
 
-        source = await ctx.fetch_message(ctx.message.reference.message_id)
+        
 
 
         # must have at least a dest arg
         # may be (dest) or (src, dest)
-        destlang = "en"
-        if len(args) != 0:
-            destlang = args[len(args) - 1]
         if destlang not in LANGUAGES:
             await ctx.reply("Error: Destination language code not valid")
             return
 
-        srclang = None
-        if len(args) == 2:
-            srclang = args[0]
         if srclang == None:
-            srclang = self.cog_translator.detect(source.content).lang
+            srclang = self.cog_translator.detect(source_message).lang
 
         if srclang not in LANGUAGES:
             await ctx.reply("Error: Source language code not valid")
             return
 
 
-        destination = self.cog_translator.translate(source.content, destlang, srclang)
-        await ctx.reply(destination.text)
+        translated = self.cog_translator.translate(source_message, destlang, srclang)
+
+        # TO DO: embed
+        await ctx.reply(translated.text)
         return
