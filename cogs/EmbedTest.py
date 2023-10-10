99274f9e2eab65d5adb085905993811f4c8d514e
AndrewLive <andrewvinhtonthat@gmail.com>
2023-10-09 19:04:57 -0700

    cog for testing embed interactivity

---
+++ cogs/EmbedTest.py
@@ -0,0 +1,18 @@
+import os, discord
+from discord.ext import commands
+import random
+
+class EmbedTest(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(aliases=['test'])
+    async def sendtest(self, ctx):
+        embed = discord.Embed(color=0xede4b2)
+
+        embed.set_author(name='LeaBot')
+        embed.set_footer(text='The Art of War - Sun Tzu')
+        embed.add_field(name='', value='This is a test embed')
+
+        await ctx.send(embed=embed)
