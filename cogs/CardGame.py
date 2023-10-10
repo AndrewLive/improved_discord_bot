a3aabf373713a2c7a8dfd0146dba39d53a22e896
AndrewLive <andrewvinhtonthat@gmail.com>
2023-10-09 19:04:36 -0700

    starting classes

---
+++ cogs/CardGame.py
@@ -0,0 +1,19 @@
+import os, discord
+from discord.ext import commands
+import random
+
+class Card():
+    def __init__(self):
+        return
+    
+
+class StandardCard(Card):
+    def __init__(self):
+        super.__init__()
+        return
+    
+
+class UnoCard(Card):
+    def __init__(self):
+        super.__init__()
+        return
