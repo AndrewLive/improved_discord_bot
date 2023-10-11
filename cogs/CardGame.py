c7a1c6d0495ac3e4718cb42f5503f920fad7714f
Andrew <andrew@puresynergy.com>
2023-10-11 07:41:54 -0700

    blackjack hand class, working on gamestate

--- cogs/CardGame.py
+++ cogs/CardGame.py
@@ -8,12 +8,164 @@ class Card():
     
 
 class StandardCard(Card):
-    def __init__(self):
-        super.__init__()
+    def __init__(self, rank, suit):
+        super().__init__()
+        self.rank = rank
+        self.suit = suit
         return
+    
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
     
 
 class UnoCard(Card):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         return
+    
+
+
+class BlackJackHand():
+    def __init__(self):
+        self.hand = []
+        self.value = 0
+        self.rank_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10, "Ace":11}
+    
+    def calculate_value(self):
+        aces = 0
+        self.value = 0
+        for card in self.hand:
+            rank = card.rank
+            self.value += self.rank_values[rank]
+            if rank == "Ace":
+                aces += 1
+        
+        while aces > 0:
+            if self.value <= 21:
+                break
+            self.value -= 10
+            aces -= 1
+        return
+    
+    def append_card(self, card:StandardCard):
+        self.hand.append(card)
+        self.calculate_value()
+        return
+    
+    def pop_card(self, rank = None)->StandardCard:
+        # if rank specified, will not remove if not found
+        card = None
+        if rank in self.rank_values.keys():
+            for c in reversed(self.hand):
+                if c.rank == rank:
+                    card = c
+                    self.hand.remove(card)
+                    break
+        else:
+            card = self.hand.pop()
+        self.calculate_value()
+        return card
+    
+
+    def __str__(self):
+        s = ''
+        s += f'({len(self.hand)} cards, value = {self.value}): '
+        for card in self.hand:
+            s += f'({card}) '
+        return s
+
+
+
+
+    
+
+# BlackJack
+class GameState():
+    def __init__(self, player):
+        self.player = player
+
+        self.player_hand = []
+        self.player_hand_value = 0
+        self.dealer_hand = []
+        self.dealer_hand_value = 0
+        self.deck = []
+        self.reset()
+
+        self.stand = False
+
+        return
+
+
+
+    def reset_deck(self):
+        self.deck.clear()
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        for rank in ranks:
+            for suit in suits:
+                card = StandardCard(rank, suit)
+                self.deck.append(card)
+
+        return
+    
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+    
+    def reset(self):
+        self.player_hand.clear()
+        self.dealer_hand.clear()
+        self.player_hand_value = 0
+        self.dealer_hand_value = 0
+        self.reset_deck()
+        self.shuffle_deck()
+
+
+
+    def calculate_value(self, hand:[StandardCard]):
+        aces = 0
+        values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10, "Ace":11}
+        value = 0
+
+        for card in hand:
+            rank = card.rank
+            value += values[rank]
+        
+        while aces > 0:
+            if value > 21:
+                value -= 10
+                aces -= 1
+        
+        return value
+    
+
+    def deal_initial_hands(self):
+        for _ in range(2):
+            card = self.deck.pop()
+            self.player_hand.append(card)
+            card = self.deck.pop()
+            self.dealer_hand.append(card)
+
+
+
+
+
+    def __str__(self):
+        s = ''
+        s += f'Player: {self.player}\n'
+
+        s += f'Player Hand ({len(self.player_hand)} cards, value = {self.player_hand_value}): '
+        for card in self.player_hand:
+            s += f'({card}) '
+        s += '\n'
+
+
+        s += f'Dealer Hand ({len(self.dealer_hand)} cards, value = {self.dealer_hand_value}): '
+        for card in self.dealer_hand:
+            s += f'({card}) '
+        s += '\n'
+
+        s += f'Deck ({len(self.deck)}): '
+        for card in self.deck:
+            s += f'({card}) '
+        s += '\n'
+        return s
---
+++ cogs/test.py
@@ -0,0 +1,40 @@
+from CardGame import Card, StandardCard, BlackJackHand, GameState
+import random
+
+if __name__ == '__main__':
+    test_hand = BlackJackHand()
+    print(test_hand)
+
+    cards = []
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    for _ in range(3):
+        rank = random.choice(ranks)
+        suit = random.choice(suits)
+        card = StandardCard(rank, suit)
+        cards.append(card)
+
+    for card in cards:
+        test_hand.append_card(card)
+
+    
+    print(test_hand)
+    test_hand.pop_card()
+    print(test_hand)
+    test_hand.pop_card()
+    print(test_hand)
+    test_hand.pop_card()
+    print(test_hand)
+
+    card = StandardCard('Ace', 'Spades')
+    test_hand.append_card(card)
+    card = StandardCard('10', 'Clubs')
+    test_hand.append_card(card)
+    card = StandardCard('2', 'Diamonds')
+    test_hand.append_card(card)
+    print(test_hand)
+
+    print(test_hand.pop_card())
+    print(test_hand)
+    print(test_hand.pop_card('Ace'))
+    print(test_hand)
