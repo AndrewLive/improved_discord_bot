6025902d98be8ff21b1df7ba8f957cd9b9c1d2f5
AndrewLive <andrewvinhtonthat@gmail.com>
2023-10-24 13:34:54 -0700

    better logic for buttons, trying to solve button-timeout bug

--- cogs/Blackjack.py
+++ cogs/Blackjack.py
@@ -47,7 +47,7 @@ class ControlButtons(discord.ui.View):
     
     # Process the reaction
     async def processButton(self, interaction, button):
-        # print("Processing Reaction")
+        print("Processing Reaction")
         if interaction.user.bot:
             return
         
@@ -74,7 +74,7 @@ class ControlButtons(discord.ui.View):
         
         if game.game_stage == 'init':
             # print(reaction.emoji)
-            if button.custom_id == 'startBtn' or button.custom_id == 'hitBtn':
+            if (button.custom_id == 'startBtn') or (button.custom_id == 'hitBtn'):
                 # print('VALID REACTION')
                 game.start_game()
 
@@ -104,7 +104,7 @@ class ControlButtons(discord.ui.View):
             return
         
         if game.game_stage == 'evaluation':
-            if button.custom_id == 'startBtn'or button.custom_id == 'hitBtn':
+            if (button.custom_id == 'startBtn') or (button.custom_id == 'hitBtn'):
                 # restart game
                 game.reset()
                 game.start_game()
