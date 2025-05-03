ebb31f389d7539329a6f46c882cdbb2a9b690f1d
Andrew <andrewvinhtonthat@gmail.com>
2025-05-02 17:14:39 -0700

    now uses python venv to keep custom package installations separate

--- cogs/SunTzu.py
+++ cogs/SunTzu.py
@@ -16,15 +16,19 @@ class SunTzu(commands.Cog):
 
 
     @commands.command(aliases=['st'])
-    async def suntzu(self, ctx, channel = 'text'):
+    async def suntzu(self, ctx, channel = 'text', musket = ''):
         """
         Gives a 100% real totally not made up quote from ancient Chinese general Sun Tzu
 
         Arguments:
             channel: Whether you want the message to be sent through text or voice (user must be connected to voice channel)
+                usage: l.st {voice/vc}
         """
 
         line = random.choice(self.lines)
+        if musket == 'musket' or channel == 'musket':
+            line = 'I own a musket for home defense, since that\'s what the founding fathers intended. Four ruffians break into my house. "What the devil?" As I grab my powdered wig and Kentucky rifle. Blow a golf ball sized hole through the first man, he\'s dead on the spot. Draw my pistol on the second man, miss him entirely because it\'s smoothbore and nails the neighbors dog. I have to resort to the cannon mounted at the top of the stairs loaded with grape shot, "Tally ho lads" the grape shot shreds two men in the blast, the sound and extra shrapnel set off car alarms. Fix bayonet and charge the last terrified rapscallion. He Bleeds out waiting on the police to arrive since triangular bayonet wounds are impossible to stitch up. Just as the founding fathers intended.'
+
 
         sun_tzu_image = 'https://almabooks.com/wp-content/uploads/2019/08/Sun-Tzu.jpg'
 
